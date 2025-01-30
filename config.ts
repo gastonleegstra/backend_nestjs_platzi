cbe6edc922c3da7aa8e5cd1e7219048607a47584
Gaston Leegstra <gleegstra@fidelatam.com>
2025-01-29 21:19:51 -0300

    Se corrige sintaxis

--- config.ts
+++ config.ts
@@ -1,15 +1,15 @@
-import { registerAs } from "@nestjs/config";
+import { registerAs } from '@nestjs/config';
 
 export default registerAs('config', () => {
   return {
     port: process.env.PORT || 3000,
-    database:{
+    database: {
       host: process.env.DATABASE_HOST,
       port: process.env.DATABASE_PORT,
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
-      type: process.env.DATABASE_TYPE
-    }
-  }
+      type: process.env.DATABASE_TYPE,
+    },
+  };
 });
